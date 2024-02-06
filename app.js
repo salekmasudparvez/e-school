f6e62ca9621d45ee7caadc4d6f8bf7e74d51f9bd
smparvez11 <parvez4954@gmail.com>
2024-02-06 16:17:13 +0600

    add js and position fixed

---
+++ app.js
@@ -0,0 +1,27 @@
+// banner text Animation 
+
+const text = document.getElementById('bannerTxt');
+const strtText = text.textContent;
+const splitText = strtText.split("")
+text.textContent="";
+
+for(let i=0; i<splitText.length; i++){
+    text.innerHTML += "<span>"+splitText[i]+"</span>";
+}
+let char = 0;
+let timer = setInterval(onclick, 90);
+
+function onclick(){
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++
+    if( char === splitText.length){
+        complete();
+        return
+    }
+}
+function complete(){
+    clearInterval(timer);
+    timer=null;
+}
+
